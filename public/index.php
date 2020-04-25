9410675fb5780f3e62cde47e4c096880567251b2
Jonathan Haist <jonathan.haist@t-online.de>
2020-04-25 11:51:34 +0200

    update for subdir

--- public/index.php
+++ public/index.php
@@ -4,6 +4,18 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
+$app->setBasePath((function () {
+    $scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+    $uri = (string) parse_url('http://a' . $_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
+    if (stripos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
+    return $_SERVER['SCRIPT_NAME'];
+    }
+    if ($scriptDir !== '/' && stripos($uri, $scriptDir) === 0) {
+    return $scriptDir;
+    }
+    return '';
+   })());
+   
 $app->get('/', function (Request $request, Response $response,
 $args) {
  $response->getBody()->write("Hello world!");
